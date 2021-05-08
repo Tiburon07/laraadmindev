dcf892d03d2666260d2d6e7822b0965cbf1bb9ec
t.iordache <tiberiu.iordache@business-changers.it>
2021-05-08 07:56:43 +0200

    welcome e home Controllers/HomeController.php

--- routes/admin.php
+++ routes/admin.php
@@ -2,7 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
-Route::get('/{name?}', function($name = ''){
-    return "ciao $name";
-});
+//Route::get('/{name?}', function($name = ''){
+//    return "ciao $name";
+//});
+
+Route::get('/', [HomeController::class,'index']);
