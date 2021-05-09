0f47c5fe67a3b54e217b0704e36bb12392349a3e
t.iordache <tiberiu.iordache@business-changers.it>
2021-05-09 18:48:46 +0200

    tolto prefisso home

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -44,6 +44,6 @@ class AuthenticatedSessionController extends Controller
         Auth::guard('web')->logout();
         $request->session()->invalidate();
         $request->session()->regenerateToken();
-        return redirect('/home');
+        return redirect('/');
     }
 }
